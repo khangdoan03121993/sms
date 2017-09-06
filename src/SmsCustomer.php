83ab3a005c598d37054e4a8f46e3194ed9cc4093
DESKTOP-CF78RPT\Admin <khangdoan03121993@gmail.com>
2017-09-06 16:16:45 +0700

    update class

--- src/SmsCustomer.php
+++ src/SmsCustomer.php
@@ -1,14 +1,23 @@
 <?php
 namespace SmsCustomer;
-class SmsCustomer
+use yii\queue\Queue;
+
+class SmsCustomer implements \yii\queue\Job
 {
+    public $mobile;
+    public $content;
+    public $apikey;
+    public $secretkey;
+    public $brandname;
+    public $typesms;
+
     public function smsCustomer($mobile, $content, $apikey, $secretkey, $brandname,$typesms){
-    	// Tạo mới một CURL
+        // Tạo mới một CURL
         $ch = curl_init();
         if($typesms == 2){
-        	$url = 'http://rest.esms.vn/MainService.svc/json/SendMultipleMessage_V4_get?Phone='.$mobile.'&Content='.$content.'&ApiKey='.$apikey.'&SecretKey='.$secretkey.'&Brandname='.$brandname.'&SmsType='.$typesms;
+            $url = 'http://rest.esms.vn/MainService.svc/json/SendMultipleMessage_V4_get?Phone='.$mobile.'&Content='.$content.'&ApiKey='.$apikey.'&SecretKey='.$secretkey.'&Brandname='.$brandname.'&SmsType='.$typesms;
         }else if($typesms == 4){
-        	$url = 'http://rest.esms.vn/MainService.svc/json/SendMultipleMessage_V4_get?Phone='.$mobile.'&Content='.$content.'&ApiKey='.$apikey.'&SecretKey='.$secretkey.'&SmsType='.$typesms;
+            $url = 'http://rest.esms.vn/MainService.svc/json/SendMultipleMessage_V4_get?Phone='.$mobile.'&Content='.$content.'&ApiKey='.$apikey.'&SecretKey='.$secretkey.'&SmsType='.$typesms;
         }
         
         // Cấu hình cho CURL
@@ -19,9 +28,39 @@ class SmsCustomer
         // curl_setopt($ch, CURLOPT_FAILONERROR, 1);
         // Thực thi CURL
         $result = [
-        			'mobile' => $mobile,
-        			'response' => curl_exec($ch)
-        			];
+                    'mobile' => $mobile,
+                    'response' => curl_exec($ch)
+                    ];
+
+        // Ngắt CURL, giải phóng
+        curl_close($ch);
+
+        return $result;
+    }
+
+    /**
+     * @param Queue $queue which pushed and is handling the job
+     */
+    public function execute($queue)
+    {
+        $url = '';
+        // Tạo mới một CURL
+        $ch = curl_init();
+        if($this->typesms == 2){
+            $url = 'http://rest.esms.vn/MainService.svc/json/SendMultipleMessage_V4_get?Phone='.$this->mobile.'&Content='.$this->content.'&ApiKey='.$this->apikey.'&SecretKey='.$this->secretkey.'&Brandname='.$this->brandname.'&SmsType='.$this->typesms;
+        }else if($this->typesms == 4){
+            $url = 'http://rest.esms.vn/MainService.svc/json/SendMultipleMessage_V4_get?Phone='.$this->mobile.'&Content='.$this->content.'&ApiKey='.$this->apikey.'&SecretKey='.$this->secretkey.'&SmsType='.$this->typesms;
+        }
+
+        // Cấu hình cho CURL
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        
+        // Thực thi CURL
+        $result = [
+            'mobile' => $this->mobile,
+            'response' => curl_exec($ch)
+        ];
 
         // Ngắt CURL, giải phóng
         curl_close($ch);
